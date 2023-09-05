71780ad92195924779e1ec7fa0c52897b493d27a
mefathim SH <git@github.com:SBK88514/test.git>
2023-09-05 13:36:20 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,2 @@
+lst = [item+2 for item in range(10)]
+print(lst)
---
+++ random.py
@@ -0,0 +1,12 @@
+import random
+import matplotlid
+
+
+class RandomWalk:
+   def __init__(self, dimensions,staps):
+       self.dim = dimensions
+       self.st = staps
+       self.coordinate_system = \
+       
+
+    rw = (RandomWolk)()
